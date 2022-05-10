ba2932e9f1aaeb32f2ceef95318c310496132874
Alexei Marusov <88613624+MarusovAlexei@users.noreply.github.com>
2022-05-10 16:29:06 +0300

    feat: add event on keyboard change language

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,24 @@
 // все клавиши английской раскладки
-const mainСharacters = ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace",
+const mainСharactersEn = ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace",
   "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\",
   "Caps lock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter",
   "Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "▲", "Shift",
   "Ctrl", "Win", "Alt", "", "Alt", "Ctrl", "◀", "▼", "▶",];
+
+// все клавиши русской раскладки
+const mainСharactersRu = ["ё", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace",
+  "Tab", "й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ", "\\",
+  "Caps lock", "ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э", "Enter",
+  "Shift", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю", ".", "▲", "Shift",
+  "Ctrl", "Win", "Alt", "", "Alt", "Ctrl", "◀", "▼", "▶",];
+
+// Коды клавиш (all 63)
+const keyCodes = ["Backquote", "Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7",
+  "Digit8", "Digit9", "Digit0", "Minus", "Equal", "Backspace", "Tab", "KeyQ", "KeyW", "KeyE", "KeyR",
+  "KeyT", "KeyY", "KeyU", "KeyI", "KeyO", "KeyP", "BracketLeft", "BracketRight", "Backslash", "CapsLock",
+  "KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyK", "KeyL", "Semicolon", "Quote", "Enter",
+  "ShiftLeft", "KeyZ", "KeyX", "KeyC", "KeyV", "KeyB", "KeyN", "KeyM", "Comma", "Period", "Slash", "ArrowUp",
+  "ShiftRight", "ControlLeft", "MetaLeft", "AltLeft", "Space", "AltRight", "ControlRight", "ArrowLeft", "ArrowDown", "ArrowRight",];
 
 // 13 special characters
 const specialСharacters = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+",];
@@ -12,30 +27,34 @@ const specialСharacters = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")
 const textBtn = ["Backspace", "Tab", "Caps lock", "Enter", "Shift", "▲", "Shift", "Ctrl", "Win",
   "Alt", "Alt", "Ctrl", "◀", "▼", "▶",];
 
-// Коды клавиш (all 63)
-const keyCodes = ["Backquote", "Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7",
-  "Digit8", "Digit9", "Digit0", "Minus", "Equal", "Backspace", "Tab", "KeyQ", "KeyW", "KeyE", "KeyR",
-  "KeyT", "KeyY", "KeyU", "KeyI", "KeyO", "KeyP", "BracketLeft", "BracketRight", "Backslash", "CapsLock",
-  "KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyK", "KeyL", "Semicolon", "Quote", "Enter",
-  "ShiftLeft", "KeyZ", "KeyX", "KeyC", "KeyV", "KeyB", "KeyN", "KeyM", "Comma", "Period", "Slash", "ArrowUp",
-  "ShiftRight", "ControlLeft", "MetaLeft", "AltLeft", "Space", "AltRight", "ControlRight", "ArrowLeft", "ArrowDown", "ArrowRight",
 
-]
+
+// Коды спецклавиш
+const keySpecialCodes = ["Backquote", "Backspace", "Tab", "CapsLock", "Enter", "ShiftLeft", "ArrowUp", "ShiftRight",
+  "ControlLeft", "MetaLeft", "AltLeft", "AltRight", "ControlRight", "ArrowLeft", "ArrowDown", "ArrowRight",];
+
+// Язык страницы
+let language = "en";
 
 // Добавляем заголовок на страницу
 document.body.append(addElemnt('Virtual keyboard', 'title', 'div'));
 
+// Создаем контейнер клавиатуры
+document.body.append(addElemnt('', 'keyboard', 'div'));
+const keyboard = document.querySelector('.keyboard');
+
 // Добавляем окно ввода текста
-document.body.append(addElemnt('', 'keyboard__display', 'textarea'));
+keyboard.append(addElemnt('', 'keyboard__display', 'textarea'));
 
 // Добавляем контейнер клавиатуры
-document.body.append(addElemnt('', 'keyboard__container', 'div'));
+keyboard.append(addElemnt('', 'keyboard__container', 'div'));
 
 // Добавляем клавиатуру
 const keyboardContainer = document.querySelector('.keyboard__container');
-addKeyboard(63);
+addKeyboard(63, mainСharactersEn, keyboardContainer);
 
-function addKeyboard(value) {
+// Функция добавления клавиатуры
+function addKeyboard(value, mainСharacters, keyboardContainer) {
   for (let i = 0; i < value; i++) {
     if (textBtn.includes(mainСharacters[i])) {
       keyboardContainer.appendChild(addElemnt(`${mainСharacters[i]}`, 'btn-dark', 'div', `${keyCodes[i]}`));
@@ -47,9 +66,15 @@ function addKeyboard(value) {
   }
 }
 
+// Функция удаления клавиатуры
+function deleteKeyboard() {
+  document.querySelector('.keyboard__container').remove();
+}
+
 // Добавляем описание
 document.body.append(addElemnt('Клавиатура создана в операционной системе Windows', 'description', 'div'));
 document.body.append(addElemnt('Для переключения языка комбинация: левыe shift + alt', 'description', 'div'));
+document.body.append(addElemnt('Для ввода текста поставьте курсор в поле ввода!', 'description', 'div'));
 
 // Функция добавления элемента на страницу ('текст', 'класс, 'тип элемента')
 function addElemnt(valueElement, classElement, elementType, elementId = 'elem') {
@@ -63,18 +88,40 @@ function addElemnt(valueElement, classElement, elementType, elementId = 'elem') 
 
 // Функция выделения кнопки при нажатии
 document.onkeydown = document.onkeyup = document.onkeypress = changeBtnClass;
+
 function changeBtnClass(event) {
+
+  // Проверяем, что кнопка нажата и добавляем класс active
   if (event.type == 'keydown') {
     document.querySelector(`#${event.code}`).classList.add('btn-active');
-    addText(event.key);
+
+    // Если кнопка цифра, символ или буква, то выводим ее
+    if (!keySpecialCodes.includes(event.code) && document.querySelector('.keyboard__display').onfocus) {
+      addText(event.key);
+    }
   }
 
-  if (event.type == 'keyup') {
+  // когда кнопка отпущена, мы убираем класс active 
+  if (event.type == 'keyup' && document.querySelector(`#${event.code}`) !== null) {
     document.querySelector(`#${event.code}`).classList.remove('btn-active');
+  }
+
+  // Переключение языка
+  if (event.shiftKey && event.altKey) {
+    deleteKeyboard();
+    keyboard.append(addElemnt('', 'keyboard__container', 'div'));
+    const keyboardContainer = document.querySelector('.keyboard__container');
+    if (language === "en") {
+      language = "ru";
+      addKeyboard(63, mainСharactersRu, keyboardContainer);
+    } else {
+      language = "en"
+      addKeyboard(63, mainСharactersEn, keyboardContainer);
+    }
   }
 }
 
-// Проверка, что ввели в textarea
+// Ввод текста в textarea
 function addText(value) {
   const textArea = document.querySelector('.keyboard__display');
   textArea.innerHTML = textArea.value + `${value}`;
